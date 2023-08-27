1a9d387178942dad15f5642a658b0c894ed32de8
Mpho_Ndlovu <126378512+MphoPetunia@users.noreply.github.com>
2023-08-27 10:08:21 +0200

    Created index.html file, as well as a src folder with app.js and style.css

---
+++ src/app.js
@@ -0,0 +1,16 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = response.data.humidity;
+}
+
+let apiKey = "ddea304a0ddf145ce143f6ot637bfd08";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Rustenburg&key=ddea304a0ddf145ce143f6ot637bfd08&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
